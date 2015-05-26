24b0a39a0210a498bf62fa1f384014ef9abcd446
Sean Emmer <sean.emmer@gmail.com>
2015-05-26 16:52:34 -0700

    push to heroku

--- config/assets/production.js
+++ config/assets/production.js
@@ -4,10 +4,18 @@ module.exports = {
 	client: {
 		lib: {
 			css: [
-
+				'public/lib/bootswatch/paper/bootstrap.css',
+				'public/lib/angular-layout/dist/angular-layout.css',
+				'public/lib/fontawesome/css/font-awesome.css'
+			
 			],
 			js: [
-
+				'public/lib/angular/angular.js',
+				'public/lib/jquery/dist/jquery.js',
+				'public/lib/angular-layout/dist/angular-layout.js',
+				'public/lib/angular-resource/angular-resource.js',
+				'public/lib/angular-ui-router/release/angular-ui-router.js',
+				'public/lib/bootstrap/dist/js/bootstrap.js'
 			]
 		},
 		css: 'public/dist/application.min.css',
--- modules/campaigns/client/controllers/campaigns.client.controller.js
+++ modules/campaigns/client/controllers/campaigns.client.controller.js
@@ -1,7 +1,13 @@
 'use strict';
 
-angular.module('campaigns').controller('CampaignsCtrl', [
-	function() {
+angular.module('campaigns').controller('CampaignsCtrl', ['$timeout',
+	function($timeout) {
+
+		$timeout(function() {
+			$('.campaigns-list-card').css(
+				'background', 'linear-gradient(rgba(0,0,0,0), rgba(0,0,0,0.4), rgba(0,0,0,0))'
+			);
+		},50);
 
 	}
 ]);
