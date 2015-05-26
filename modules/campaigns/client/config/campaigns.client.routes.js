daa7a5d2965a65bf107d40b8aec5104d95c45a70
Sean Emmer <sean.emmer@gmail.com>
2015-05-26 16:11:46 -0700

    completed campaigns templates, favicons

--- modules/campaigns/client/config/campaigns.client.routes.js
+++ modules/campaigns/client/config/campaigns.client.routes.js
@@ -5,8 +5,24 @@ angular.module('campaigns').config(['$stateProvider',
 		// Campaign state routing
 		$stateProvider
 			.state('campaigns', {
+				abstract: true,
 				url: '/campaigns',
 				templateUrl: 'modules/campaigns/views/campaigns.client.view.html',
+				controller: 'CampaignsCtrl'
+			})
+			.state('campaigns.list', {
+				url: '',
+				templateUrl: 'modules/campaigns/views/partials/campaigns.list.client.view.html',
+				controller: 'CampaignsCtrl'
+			})
+			.state('campaigns.show', {
+				url: '/1',
+				templateUrl: 'modules/campaigns/views/partials/campaigns.show.client.view.html',
+				controller: 'CampaignsCtrl'
+			})
+			.state('campaigns.offers', {
+				url: '/1/offers',
+				templateUrl: 'modules/campaigns/views/partials/campaigns.offers.client.view.html',
 				controller: 'CampaignsCtrl'
 			});
 	}
